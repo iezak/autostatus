148ad194a81aec7e2df8dd0d0a45f242775ab419
iezak <56704008+iezak@users.noreply.github.com>
2025-12-17 08:46:36 -0300

    Delete inc directory

--- inc/config.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Simple helper for config UI.
- */
-class PluginAutostatusConfig {
-   public static function canUpdate(): bool {
-      return Session::haveRight('config', UPDATE);
-   }
-}
